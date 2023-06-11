0865fe3d8f7e28bde57a0051e3b43f82fd2d42f5
Cedric Boucher <onebitalpha@gmail.com>
2023-06-10 21:36:16 -0600

    improved code

--- HWINFO_plot.py
+++ HWINFO_plot.py
@@ -2,29 +2,46 @@ import csv
 import matplotlib.pyplot as plt
 from datetime import datetime
 
-filenames = [#"Win10 Normal OS/June 9 2023 Prime95 Torture Test Small FFTs Case Closed.CSV",
-             #"Win10 Normal OS/June 9 2023 Prime95 Torture Test Small FFTs No Sidepanels.CSV",
-             #"Win10 Normal OS/June 10 2023 Prime95 Torture Test Small FFTs No Sidepanels or front or top.CSV",
-             #"Win10 Normal OS/June 10 2023 Prime95 Torture Test Small FFTs No Sidepanels or front or top no blowie.CSV",
-             "Win11 Benchmark OS/June 10 2023 Prime95 no blower fan.CSV",
+"""
+filenames = ["Win10 Normal OS/June 9 2023 Prime95 Torture Test Small FFTs Case Closed.CSV",
+             "Win10 Normal OS/June 9 2023 Prime95 Torture Test Small FFTs No Sidepanels.CSV",
+             "Win10 Normal OS/June 10 2023 Prime95 Torture Test Small FFTs No Sidepanels or front or top.CSV",
+             "Win10 Normal OS/June 10 2023 Prime95 Torture Test Small FFTs No Sidepanels or front or top no blowie.CSV"]
+"""
+"""
+filenames = ["Win11 Benchmark OS/June 10 2023 Prime95 no blower fan.CSV",
              "Win11 Benchmark OS/June 10 2023 Prime95 no sidepanels no blower fan.CSV",
              "Win11 Benchmark OS/June 10 2023 Prime95 no sidepanels no top no front no blower fan.CSV",
-             "Win11 Benchmark OS/June 10 2023 Cinebench R23 no sidepanels no top no front no blower fan.CSV"]
+             "Win11 Benchmark OS/June 10 2023 Prime95 5GHz New Fan Curve No Blower.CSV",
+             "Win11 Benchmark OS/June 10 2023 Prime95 New Fan Curve No Blower.CSV"]
+"""
+filenames = ["Laptop Win10/June 10 2023 Laptop Performance Mode Prime95.CSV",
+             "Laptop Win10/June 10 2023 Laptop Standard Mode Prime95.CSV",
+             "Laptop Win10/June 10 2023 Laptop Whisper Mode Prime95.CSV"]
 
-legend = [#"Prime95 Case Closed",
-          #"Prime95 No Sidepanels",
-          #"Prime95 No Sidepanels, Top, or Front Panel",
-          #"Prime95 No Sidepanels, Top, or Front Panel, No Blower Fan",
-          "Win11 Prime95 No Blower Fan",
+"""
+legend = ["Prime95 Case Closed",
+          "Prime95 No Sidepanels",
+          "Prime95 No Sidepanels, Top, or Front Panel",
+          "Prime95 No Sidepanels, Top, or Front Panel, No Blower Fan"]
+"""
+"""
+legend = ["Win11 Prime95 No Blower Fan",
           "Win11 Prime95 No Sidepanels, No Blower Fan",
           "Win11 Prime95 No Sidepanels, Top, or Front Panel, No Blower Fan",
-          "Win11 Cinebench R23 No Sidepanels, Top, or Front Panel, No Blower Fan"]
+          "Win11 Prime95 5GHz New Fan Curve No Blower Fan",
+          "Win11 Prime95 New Fan Curve No Blower Fan"]
+"""
+legend = ["Laptop Prime95 Performance Mode",
+          "Laptop Prime95 Standard Mode",
+          "Laptop Prime95 Whisper Mode"]
+
 
 number_of_runs = len(filenames)
 
-max_seconds = 600
+max_seconds = 600 # the average still uses points after this, this will set xlim
 
-ignore_first_n_datapoints_for_average = 15
+ignore_first_n_datapoints_for_average = 20
 
 assert (len(filenames) == len(legend)), "different number of filenames and legends"
 
@@ -34,59 +51,56 @@ cpu_temperature_deltas: list[list[float]] = []
 cpu_powers: list[list[float]] = []
 timestamps: list[list[float]] = []
 
+
+def find_index(string: str, list_to_look_in: list[str]) -> int:
+    """
+    finds the index in the list which contains string
+    """
+    index = 0
+    for item in list_to_look_in:
+        if item.find(string) != -1:
+            break
+        index += 1
+
+    return index
+
+
 i = -1
 for filename in filenames:
     i += 1
     with open(filename, 'r') as file:
         reader = csv.reader(file)
-        rows = [row for row in reader]
+        rows = [row for row in reader] # read the whole file
 
-        cpu_temp_column = 0
-        for item in rows[0]:
-            if item.find("CPU Package") != -1:
-                break
-            cpu_temp_column += 1
-        
-        room_temp_column = 0
-        for item in rows[0]:
-            if item.find("T_Sensor1") != -1:
-                break
-            room_temp_column += 1
+    # find column indices of variables
+    cpu_temp_column = find_index("CPU Package", rows[0])
+    room_temp_column = find_index("T_Sensor1", rows[0])
+    cpu_power_column = find_index("CPU Package Power", rows[0])
 
-        cpu_power_column = 0
-        for item in rows[0]:
-            if item.find("CPU Package Power") != -1:
-                break
-            cpu_power_column += 1
+    initial_time = datetime.strptime(rows[1][0] + ' ' + rows[1][1].split(".")[0], '%d.%m.%Y %H:%M:%S')
 
-        initial_time = datetime.strptime(rows[1][0] + ' ' + rows[1][1].split(".")[0], '%d.%m.%Y %H:%M:%S')
+    # Initialize lists to store the data
+    timestamps.append([])
+    cpu_temperatures.append([])
+    room_temperatures.append([])
+    cpu_temperature_deltas.append([])
+    cpu_powers.append([])
 
-        # Initialize lists to store the data
-        timestamps.append([])
-        cpu_temperatures.append([])
-        room_temperatures.append([])
-        cpu_temperature_deltas.append([])
-        cpu_powers.append([])
+    for row in rows[1:]:
+        try:
+            timestamp = (datetime.strptime(row[0] + ' ' + row[1].split(".")[0], '%d.%m.%Y %H:%M:%S') - initial_time).total_seconds() # Combine date and time columns
+        except ValueError:
+            continue # the last couple rows are headers for some reason, so datetime doesn't know how to convert a string of characters to time
 
-        # Read the data from the CSV file
-        for row in rows[1:]:
-            # Extract the relevant columns
-            try:
-                timestamp = (datetime.strptime(row[0] + ' ' + row[1].split(".")[0], '%d.%m.%Y %H:%M:%S') - initial_time).total_seconds() # Combine date and time columns
-                if timestamp > max_seconds:
-                    break # stop reading rows so all the lines end at the same time
-                cpu_temp = float(row[cpu_temp_column])  # CPU Package temperature column
-                cpu_power = float(row[cpu_power_column])
-                room_temp = float(row[room_temp_column])
+        cpu_temp = float(row[cpu_temp_column])
+        cpu_power = float(row[cpu_power_column])
+        room_temp = float(row[room_temp_column])
 
-                # Append the data to the lists
-                timestamps[i].append(timestamp)
-                cpu_temperatures[i].append(cpu_temp)
-                cpu_temperature_deltas[i].append(cpu_temp - room_temp)
-                cpu_powers[i].append(cpu_power)
-            except ValueError:
-                #print("row time format did not match expected")
-                pass
+        # Append the data to the lists
+        timestamps[i].append(timestamp)
+        cpu_temperatures[i].append(cpu_temp)
+        cpu_temperature_deltas[i].append(cpu_temp - room_temp)
+        cpu_powers[i].append(cpu_power)
 
 
 legend = [legend[int(i/2)] if i%2 == 0 else "Average" for i in range(len(legend)*2)]
@@ -97,7 +111,7 @@ for i in range(number_of_runs):
     plt.scatter(timestamps[i], cpu_temperatures[i], s = 3)
 
     average_y = sum(cpu_temperatures[i][ignore_first_n_datapoints_for_average:]) / len(cpu_temperatures[i][ignore_first_n_datapoints_for_average:])
-    start_end_x = (timestamps[i][0], timestamps[i][-1])
+    start_end_x = (timestamps[i][0], max_seconds)
     plt.plot(start_end_x, [average_y, average_y], linewidth = 3) # average temp "trendline"
 
     plt.ylabel('CPU Package Temperature (°C)')
@@ -105,7 +119,7 @@ for i in range(number_of_runs):
     plt.xticks(rotation=45)
     plt.yticks(tuple(range(10, 105, 5)))
     plt.ylim((10, 105))
-    plt.xlim((0, timestamps[i][-1]))
+    plt.xlim((0, max_seconds))
     plt.tight_layout()
 
 plt.legend(legend)
@@ -117,7 +131,7 @@ for i in range(number_of_runs):
     plt.scatter(timestamps[i], cpu_temperature_deltas[i], s = 3)
 
     average_y = sum(cpu_temperature_deltas[i][ignore_first_n_datapoints_for_average:]) / len(cpu_temperature_deltas[i][ignore_first_n_datapoints_for_average:])
-    start_end_x = (timestamps[i][0], timestamps[i][-1])
+    start_end_x = (timestamps[i][0], max_seconds)
     plt.plot(start_end_x, [average_y, average_y], linewidth = 3) # average temp "trendline"
 
     plt.xlabel('Time')
@@ -126,7 +140,7 @@ for i in range(number_of_runs):
     plt.xticks(rotation=45)
     plt.yticks(tuple(range(0, 90, 5)))
     plt.ylim((0, 90))
-    plt.xlim((0, timestamps[i][-1]))
+    plt.xlim((0, max_seconds))
     plt.tight_layout()
 
 plt.legend(legend)
@@ -141,7 +155,7 @@ for i in range(number_of_runs):
     plt.scatter(timestamps[i], cpu_powers[i], s = 3)
 
     average_y = sum(cpu_powers[i][ignore_first_n_datapoints_for_average:]) / len(cpu_powers[i][ignore_first_n_datapoints_for_average:])
-    start_end_x = (timestamps[i][0], timestamps[i][-1])
+    start_end_x = (timestamps[i][0], max_seconds)
     plt.plot(start_end_x, [average_y, average_y], linewidth = 3) # average temp "trendline"
 
     plt.xlabel('Time')
@@ -150,7 +164,7 @@ for i in range(number_of_runs):
     plt.xticks(rotation=45)
     plt.yticks(tuple(range(0, 200, 5)))
     plt.ylim((0, 200))
-    plt.xlim((0, timestamps[i][-1]))
+    plt.xlim((0, max_seconds))
     plt.tight_layout()
 
 plt.legend(legend)
